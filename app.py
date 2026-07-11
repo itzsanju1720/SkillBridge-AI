cd8cc65d3b7fd79aad8a2cbf2dafc99cbccf8ece
sanju <sahnisanjana14@gmail.com>
2026-07-11 07:21:25 +0530

    Improved AI response speed and updated project

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 app = Flask(__name__)
 app.secret_key = "skillbridge_secret"
